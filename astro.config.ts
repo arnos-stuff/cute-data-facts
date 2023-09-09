563f2c72042ccf0d36b79f9747092570d726d719
arnos-stuff <bcda0276@gmail.com>
2023-09-09 20:00:27 +0200

    Added first issue

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-import fs from 'fs';
+import fs from "fs";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
@@ -10,44 +10,44 @@ import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	// ! Please remember to replace the following site property with your own domain
-	site: "https://astro-theme-cactus.netlify.app/",
-	markdown: {
-		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
-		remarkRehype: { footnoteLabelProperties: { className: [""] } },
-		shikiConfig: {
-			theme: "dracula",
-			wrap: true,
-		},
-	},
-	integrations: [
-		mdx({}),
-		tailwind({
-			applyBaseStyles: false,
-		}),
-		sitemap(),
-		prefetch(),
-	],
-	vite: {
-		plugins: [rawFonts(['.ttf'])],
-		optimizeDeps: {
-			exclude: ["@resvg/resvg-js"],
-		},
-	},
+  // ! Please remember to replace the following site property with your own domain
+  site: "https://qtdata.shae.run/",
+  markdown: {
+    remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
+    remarkRehype: { footnoteLabelProperties: { className: [""] } },
+    shikiConfig: {
+      theme: "rose-pine-moon",
+      wrap: true,
+    },
+  },
+  integrations: [
+    mdx({}),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+    prefetch(),
+  ],
+  vite: {
+    plugins: [rawFonts([".ttf"])],
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+  },
 });
 
 function rawFonts(ext: Array<string>) {
   return {
-    name: 'vite-plugin-raw-fonts',
-		// @ts-ignore:next-line
+    name: "vite-plugin-raw-fonts",
+    // @ts-ignore:next-line
     transform(_, id) {
-      if (ext.some(e => id.endsWith(e))) {
+      if (ext.some((e) => id.endsWith(e))) {
         const buffer = fs.readFileSync(id);
         return {
           code: `export default ${JSON.stringify(buffer)}`,
-          map: null
+          map: null,
         };
       }
-    }
+    },
   };
 }
